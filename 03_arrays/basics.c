99c9aa00a02ab64ed3356488411937d60e6e863c
Benjamin Ewert <lars@vmmint13-ss14.(none)>
2014-11-10 17:01:03 +0100

    aktualisierter Stand nach Tutorium

--- 03_arrays/basics.c
+++ 03_arrays/basics.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
@@ -10,20 +11,28 @@ int main(){
    * einfaches Array inkl. Zugriffen
    */
 
-  int arr1[5] = {1,2,3,4,5};
+  int arr1[10] = {1,2,3,4,5};
 
   
   // Ausgabe der einzelnen Elemente
+ for (i = 0; i < 10; i++){ 
+    printf("Am Index %d liegt der Wert %d\n",i, arr1[i] );
+ }
   
-    printf("Am Index %d liegt der Wert %d\n",i, i );
-  
-  /*
   int arr[10] = {0,1,2,3,4,5,12,13,42,10};
-  int sum;
+  int sum =0;
 
   // durchschnitt berechnen
+  for (i = 0; i < 10; i++){
+    sum = sum + arr[i];
+  }
 
   printf("Durchschnitt: %f\n", sum/10.0);
-*/
+
+  char arr3[] = {'d', 'u', 'r', ' ', 't','t'};
+
+  for (i = 0; i < strlen(arr3);i++){
+    printf("%c",arr3[i]); 
+  }
   return 0;
 }
--- 03_arrays/char.c
+++ 03_arrays/char.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(){
   char text[] = "Ich bin ein Text ohne Sinn, aber mit den Zahlen 0,8,1,5";
   int i = 0;
-
+/*
   while(text[i] != '\0'){
     printf("%c - %3d\n", text[i], text[i]);
     i++;
+  }
+
+  int bsp = '8' - '0';
+
+  printf("%d\n", bsp);
+*/
+  char arr[] = "hallo";
+
+  int j = 0;
+
+
+  for ( i = 0; i < strlen(arr); i++) {  // Schleife 1
+    for (j = 0; j < i; j++) {           // Schleife 2
+      printf("*");
+    }
+    
+    printf("%c", arr[i]);
+
+    for (j++; j < strlen(arr); j++) {    // Schleife 3
+      printf("*");
+    }
+
+    printf("\n");
   }
 
   return 0;
