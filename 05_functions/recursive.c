969c0c9e15bcf9f91c8b60cc72c0605bf1164646
Benjamin Ewert <bee9156@thi.de>
2014-12-01 17:31:28 +0100

    cleaned up recursive.c

--- 05_functions/recursive.c
+++ 05_functions/recursive.c
@@ -7,9 +7,7 @@ Diese Datei enthält ein paar rekursive Funktionen
 
 // Vorwärtdeklarationen
 int rec_faculty(int a);
-int it_faculty(int a);
 int rec_array_sum(int* arr, int i);
-int it_array_sum(int* arr);
 
 // definitionen
 int rec_faculty(int a){
@@ -38,70 +36,9 @@ int main(){
 	result = rec_faculty(ende);
 	printf("Die Fakultät von %d ist %d (rekursiv)\n", ende, result);
 
-	result = it_faculty(ende);
-	printf("Die Fakultät von %d ist %d (iterativ)\n", ende, result);
-
 	result = rec_array_sum(arr, 0);
 	printf("Die Summe des Arrays ist %d (rekursiv)\n", result);
-
-	result = it_array_sum(arr);
-	printf("Die Summe des Arrays ist %d (iterativ)\n", result);
-
-    printf("%d\n", arr[7]);
 	
 
 	return 0;
 }
-
-
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-int rec_faculty(int a) {
-	if (a == 0) {
-		return 1;
-	} else {
-		return a * rec_faculty(a-1);
-	}
-}*/
-
-int it_faculty(int a) {
-	int result = 1;
-
-	while (a > 0) {
-		result = result * a;
-		a--;
-	}
-
-	return result;
-}
-
-/*int rec_array_sum(int* arr, int i) {
-	if (i >= 0) {
-		return 	arr[i] + rec_array_sum(arr, i -1);
-	} else {
-		return 0;
-	}
-}*/
-
-int it_array_sum(int* arr) {
-	int i;
-	int result = 0;
-
-	for (i = 0; arr[i] != '\0'; i++) {
-		result = result + arr[i];
-	}
-
-	return result;
-}	
