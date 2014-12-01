12d26803769c3063deb5048297327403559837d5
Benjamin Ewert <bee9156@thi.de>
2014-12-01 11:22:39 +0100

    reworked the templates

--- XX_templates/header_template.h
+++ XX_templates/header_template.h
@@ -9,55 +9,60 @@ _TEMPLATE_H ist ein eindeutiger Name. Es handelt sich hier um eine globale Defin
 aber ohne Inhalt (der wird aber auch nicht benötigt).
 */
 
-// 2. Include der in dieser Datei benötigten Frameworkheader (z.B. Datentypen)
+
+
+// 2. Include der in dieser Headerdatei benötigten Frameworkheader (z.B. Datentypen)
 // Erklärung: siehe source_template.c
 //#include <...>
 
-// 3. Include der in dieser Datei benötigten selbst geschriebenen Header (z.B.: typedefs, structs, etc)
+
+
+// 3. Include der in dieser Headerdatei benötigten selbst geschriebenen Header (z.B.: typedefs, structs, etc)
 // Erklärung: siehe source_template.c
 //# include "..."
+
+
 
 // 4. Globale defines
 /*
 Diese Defines sind global (von allen anderen externen Source und Headerdateien) sichtbar.
 Anwendungsbeispiele:
 * global benötigte Puffergrößen
-* global benötigte Konstanten anderer Art
+* global benötigte Zählergrößen, etc
 
 Da diese Defines überall sichtbar sind, könnten Probleme mit gleichnamigen internen defines entstehen.
 Um dies zu vermeiden (und der besseren Lesbarkeit halber) bietet es sich an den Prefix "GLOBAL_" zu
 verwenden. Global sichtbare defines müssen einen eindeutigen Namen besitzen und dürfen nur einmal erstellt werden!!!
 Unbedingt sinnvolle und einzigartige Namen vergeben!
 */
-#define GLOBAL_DEFINE "ich bin überall Sichtbar"
+#define GLOBAL_MODULNAME_DEFINE "ich bin überall Sichtbar"
+
+
 
 // 5. Globale Variablen
 /*
-Diese Variablen sind global sicht und müssen daher einen eindeutigen Namen besitzen.
-Sie können von extern verändert werden.
-
-Um einen besser lesbaren Code zu schreiben, sollten diese mit dem Prefix "global_" versehen werden.
+Da in Headerdateien Varaiablen eig nichts zu suchen haben machen wir hier nur unsere in der Sourcedatei
+angelegten globalen Variablen für alle sichtbar
 */
-int global_counter;
+extern int global_template_counter;
 
-// 6. Globale Bekanntmachung von Variablen, welche in der eigenen Sourcedatei angelegt wurden.
-// Erklärung: source_template.c
-// Hinweis: sehr unsauberes Coding, dies sollte vermieden werden!!!
-extern int internal_counter;
 
-// 7. öffentliche Funktionen der .c Datei (können extern verwendet werden)
+
+// 6. öffentliche Funktionen der .c Datei (können extern verwendet werden)
 /*
-Alle hier beschriebenen Funktionen sind global sichtbar, daher muss auch hier ein eindeutiger
-Name vergeben werden.
+Alle hier denklarierten Funktionen sind global sichtbar (Erklärung: siehe Sourcedatei)
 
 Für eine gute Lesbarkeit bietet sich eine thematische Aufteilung an. Die Reihenfolge ist hier ziemlich
 egal und kann sich von der Sourcedatei unterscheiden - auch wenn es sinnvoll ist die gleiche Reihenfolge
 zu verwenden.
 
 Beispiel einer Aufteilung: source_template.c
+ 
+Die Name, Parameter und Rückgabewerte müssen identisch zu der Funktionsdefinition in der Sourcedatei sein,
+sonst wird die hier angelegte Funktion als eigenständig betrachtet
 */
-void normal_function1();
-void normal_function2();
+void template_normal_function1();
+void template_normal_function2();
 
 // Ende des Include Schutzes
 #endif
--- XX_templates/source_template.c
+++ XX_templates/source_template.c
@@ -1,48 +1,85 @@
 // 1. includen des entsprechenden Headerfiles
 #include "header_template.h"	// die "" werden benötigt da das headerfile von uns erstellt wurde
 
+
+
 // 2. includen der benötigten framework header:
 /*
-Die <> Klammer werden bei Frameworkheadern verwendet. Diese zeigen dem Compiler, das die entsprechenden Header im Bibliotheksverzeichnis des Betriebssystem liegen.
-Damit Header auf diese Art aufgerufen werden können werden die Header normalerweise aus der Betriebssystemsrepository (z.B.: durch apt-get / aptitude) installiert. Bei Windows ähnlich.
+Die <> Klammer werden bei Frameworkheadern verwendet. Diese zeigen dem Compiler, das die entsprechenden Header im
+Bibliotheksverzeichnis des Betriebssystem liegen.
+Damit Header auf diese Art aufgerufen werden können werden die Header normalerweise aus der
+Betriebssystemsrepository (z.B.: durch apt-get / aptitude) installiert. Bei Windows ähnlich.
 
-Includes werden immer in der Datei (Header/Source) durchgeführt, an der Sie als erstes tatsächlich verwendet werden.
+Includes werden immer in der Datei (Header/Source) durchgeführt, an der Sie als erstes tatsächlich verwendet
+werden.
 */
 //#include <stdio.h>
+
+
 
 // 3. includen weiterer selbstgeschriebener header
 //#include "weiterer_header.h"		// um es einfach zu halten: alle header- und sourcefiles sollten im gleichen ordner liegen
 
+
+
 // 4. interne defines
 /*
-interne defines sind nur innerhalb der entsprechenden Sourcedatei sichtbar. Da beim compilieren die Daten nur innerhalb verwendet werden, sollten!!!!!! keine Probleme bei einem erneuten define mit identischem Namen, aber unterschiedlichem Wert entstehen.
-*/
-#define INTERNAL_DEFINE "Ich bin nur hier sichtbar"
+interne defines sind nur innerhalb der entsprechenden Sourcedatei sichtbar.
+Falls ein anderes Modul (Source + Header) intern einen identischen (aber unterschiedliche Werte) define
+bereitstellen, gibt es keine Probleme, sollte allerdings an irgend einer Stelle ausserhalb dieser Datei
+ein globales define (in der Headerdatei) angelegt werden, so gibt es Probleme da der Compiler nicht weiss
+welche Variante er benutzen soll.
 
-// 5. intern - globale variablen
-/*
-Diese Variablen sind für alle in dieser Datei verwendeten Funktionen sichtbar, bzw. können von diesen geändert
-werden.
-Da hier allerdings Probleme mit globalen Variablen entstehen können bietet sich ein Prefix an ("internal_")
-Soll eine Variable komplett global zugänglich sein, so sollte diese in der Headerdatei anstelle hier
-hinterlegt werden.
+Daher bietet sich ein Prefix mit dem Schema INTERNAL_<MODULNAME>_ an.
 */
-int internal_counter;
+#define INTERNAL_MODULNAME_DEFINE "Ich bin nur hier sichtbar"
 
-// 6. Vorwärtsdeklaration interner Hilfsfunktionen:
+
+
+// 5. globale Variablen - extern
 /*
-interne Vorwärsdeklarationen sind nur innerhalb der entsprechenden Sourcedatei sichtbar (kann nicht von ausserhalb verwendet werden).
+Diese Variablen sind für alle anderen Sourcedateien in unserem Programm sichtbar.
+Da diese mit anderen Variablen in unserem Programm kollidieren können, muss ihnen zwingend ein einmaliger
+Name gegeben werden.
+Um diese Variablen ausserhalb bekannt zu machen müssen sie in unserer Headerdatei deklariert werden.
+
+Um einen einzigartigen Namen zu gewährleisten biete sich hier das Prefix global_<modulname>_ an
+*/
+int global_template_counter;
+
+
+
+// 6. globale variablen - intern
+/*
+Diese Variablen sind für alle in dieser Datei verwendeten Funktionen sichtbar, allerdings nicht für andere
+Module unseres Programms. Da hier allerdings Probleme mit globalen Variablen entstehen können bietet sich
+ein Prefix an ("internal_<modulname>_").
+*/
+int internal_template_counter;
+
+
+
+// 7. Vorwärtsdeklaration interner Hilfsfunktionen:
+/*
+interne Vorwärsdeklarationen sind nur innerhalb der entsprechenden Sourcedatei sichtbar (können nicht von
+ausserhalb verwendet werden).
 
 WICHTIG:
-Anders als bei internen Defines entstehen bei Mehrfachverwendung in verschiedenen Dateien Probleme beim Verlinkungsvorgang beim compilieren!!!
+Trotz dieser eingeschränkten Sichtbarkeit merkt sich der Compiler diese. Das führt dazu das er sich beim
+compilieren nicht mehr auskennt welche Funktion er jetzt genau verwenden soll.
 
-Ein möglicher Work-Around dafür ist die Nutzung eines Prefixes (z.b.: die Anfangsbuchstaben der Sourcedate - st_):
+Hier bietet sich ein Prefix nach dem Schema h_<modulname>_ an. ( h = helper)
 */
-void st_function_name1();
-void st_function_name2();
+void h_template_function_name1();
+void h_template_function_name2();
 
-// 7. gecodete Funktionen (aus headerdatei)
+
+
+// 8. gecodete Funktionen
 /*
+Diese Funktionen sollten in der Headerdatei deklariert werden. Dies führt dazu das diese Funktionen
+in anderen Modulen, wie auch hier sichtbar werden.
+ 
 Um die Lesbarkeit zu verbessern bietet es sich an die Funktionen thematisch auszuteilen.
 Bsp.:
 // initialisierung & Speicherkram;
@@ -61,19 +98,24 @@ void do_something_else();
 .
  
 */
-void normal_function1(){
+void template_normal_function1(){
 	// hier steht der Code
 }
 
-void normal_function2(){
+void template_normal_function2(){
 	// hier steht der Code
 }
 
-// 8. gecodete Hilfsfunktionen
-void st_function_name1(){
+
+
+// 9. gecodete Hilfsfunktionen
+/*
+hier coden wir die oben bereits deklarierten Hilfsfunktionen
+*/
+void h_template_function_name1(){
 	// hier steht der Code
 }
 
-void st_function_name2(){
+void h_template_function_name2(){
 	// hier steht der Code
 }
